1a461c5c760e6e3fe7a05c8b3b7c29cd8429c5d7
jonique98 <tnals9899@naver.com>
2023-08-19 10:16:00 +0900

    login

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const bodyparser = require('body-parser')
 const methodOverride = require('method-override')
 app.use(express.urlencoded({extended: true}));
-const mongoURL = 'mongodb+srv://sumin9899:su1214@cluster0.1nanggh.mongodb.net/?retryWrites=true&w=majority'
 app.set('view engine', 'ejs');
+require('dotenv').config();
 
 const MongoClient = require('mongodb').MongoClient;
 
@@ -12,18 +12,22 @@ app.use('/public', express.static('public'));
 app.use(methodOverride('_method'))
 
 var db;
-MongoClient.connect(mongoURL, function(error, client){
+MongoClient.connect(process.env.mongoURL, function(error, client){
     
     if(error) return console.log(error);
     db = client.db('todoapp');
 
-    app.listen('8080', function(){
+    app.listen(process.env.PORT, function(){
       console.log('listening on 8080')
     });
 
     app.get('/', function(req, res){
-        res.render('index.ejs');
+        res.render('login.ejs');
     });
+
+    app.get('/home', function(req, res){
+      res.render('index');
+  });
     
     app.get('/write', function(req, res){
         res.render('write.ejs');
@@ -101,3 +105,65 @@ MongoClient.connect(mongoURL, function(error, client){
     // })
 
   })
+
+
+  const passport = require('passport');
+  const LocalStrategy = require('passport-local');
+  const session = require('express-session');
+
+  app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+  app.use(passport.initialize());
+  app.use(passport.session()); 
+
+  app.get('/login', function(req, res){
+    res.render('login.ejs')
+  })
+
+  app.post('/login', passport.authenticate('local', {
+    failureRedirect : '/fail'
+  }),function(req, res){
+    res.redirect('/home');
+  });
+
+  app.get('/mypage', checkLogin, function(req, res){
+    console.log(req.user)
+    res.render('mypage.ejs', {사용자 : req.user});
+  })
+
+  function checkLogin(req, res, next){
+    if (req.user){
+        next();
+    }else {
+        res.send('로그인 안하셨는데요..')
+    }
+    
+  }
+
+  passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw', 
+    session: true,
+    passReqToCallback: false,
+  }, function (입력한아이디, 입력한비번, done) {
+    console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+      if (에러) return done(에러) 
+
+      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+      if (입력한비번 == 결과.pw) {
+        return done(null, 결과)
+      } else {
+        return done(null, false, { message: '비번틀렸어요' })
+      }
+    })
+  }));
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id)
+  });
+  
+  passport.deserializeUser(function (아이디, done) {
+    db.collection('login').findOne({id : 아이디}, function(error, result){
+      done(null, result);
+    })
+  }); 
